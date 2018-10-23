44545cecf76f8d19c3ce8acba5694d04af892600
thimtkfkd05 <tgoon486@kaist.ac.kr>
2018-10-24 01:48:52 +0900

    Update KakaoTalk.java
    added try-catch in init for catching AlreadyInitializedException

--- src/android/KakaoTalk.java
+++ src/android/KakaoTalk.java
@@ -52,7 +52,11 @@ public class KakaoTalk extends CordovaPlugin {
         Log.v(LOG_TAG, "kakao : initialize");
         super.initialize(cordova, webView);
         currentActivity = this.cordova.getActivity();
-        KakaoSDK.init(new KakaoSDKAdapter());
+	try {
+            KakaoSDK.init(new KakaoSDKAdapter());
+	} catch(KakaoSDK.AlreadyInitializedException e) {
+	    Log.v(LOG_TAG, "kakao : already initialized");
+	}
     }
 
     /**
